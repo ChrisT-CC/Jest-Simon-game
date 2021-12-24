66434e7da9e8e7d8ca8e638143b515d31211257e
ChrisT-CC <cristaecc@gmail.com>
2021-12-24 19:40:46 +0000

    Set up the test file with defaults to load the HTML file to the DOM

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,22 @@
 /**
  * @jest-environment jsdom
  */
+/* 
+Change the default Node.js environment to jsdom (a browser-like environment, 
+used when building a web app)
+By adding a @jest-environment docblock at the top of the file 
+*/
+
+// Load the entire HTML page and attach it to the mock DOM
+beforeAll(() => {
+    /* Install Node's fs library (a file system handling module built into Node.js 
+    that allows us to open, read and write files) */
+    let fs = require("fs");
+    /* Use fs to read the contents of index.html file, with the utf-8 character set 
+    and store it in fileContents variable */
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    /* Attach the fileContents to the DOM */
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
